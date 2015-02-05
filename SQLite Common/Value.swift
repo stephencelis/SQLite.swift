8ba08493bb20b926e8dd7bc968e2defe888a72ed
Stephen Celis <stephen@stephencelis.com>
2015-02-04 21:01:08 -0800

    2015: The year of header cleanup
    Let's update our copyright to reflect the current year.
    
    Let's get rid of licenses on files that don't need them.
    
    Let's include the project's URL.
    
    Let's make other minor changes.
    
    Signed-off-by: Stephen Celis <stephen@stephencelis.com>

--- SQLite Common/Value.swift
+++ SQLite Common/Value.swift
@@ -1,6 +1,7 @@
 //
-// SQLite.Value
-// Copyright (c) 2014 Stephen Celis.
+// SQLite.swift
+// https://github.com/stephencelis/SQLite.swift
+// Copyright (c) 2014-2015 Stephen Celis.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
