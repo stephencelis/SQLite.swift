2b3780cccac7cb9d38b3d887ec8515b265c490b6
Russ Bishop <xenadu@users.noreply.github.com>
2015-03-04 10:22:12 -0800

    Support non-integer primary key in shorthand function

--- SQLite Tests/SchemaTests.swift
+++ SQLite Tests/SchemaTests.swift
@@ -7,6 +7,7 @@ private let age = Expression<Int?>("age")
 private let salary = Expression<Double>("salary")
 private let admin = Expression<Bool>("admin")
 private let manager_id = Expression<Int64?>("manager_id")
+private let uniqueIdentifier = Expression<UniqueIdentifier>("uniqueIdentifier")
 
 class SchemaTests: XCTestCase {
 
@@ -42,6 +43,18 @@ class SchemaTests: XCTestCase {
             db.create(table: users) { t in
                 t.column(email)
             }
+        )
+    }
+
+    func test_createTable_column_nonIntegerPrimaryKey() {
+        ExpectExecution(db, "CREATE TABLE \"users\" (\"email\" TEXT PRIMARY KEY NOT NULL)",
+            db.create(table: users) { $0.column(email, primaryKey: true) }
+        )
+    }
+    
+    func test_createTable_column_customTypePrimaryKey() {
+        ExpectExecution(db, "CREATE TABLE \"users\" (\"uniqueIdentifier\" TEXT PRIMARY KEY NOT NULL)",
+            db.create(table: users) { $0.column(uniqueIdentifier, primaryKey: true) }
         )
     }
 
