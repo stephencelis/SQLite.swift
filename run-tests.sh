02cb799aada44eb56a848391455fa63cea3de29d
Jan Berkel <jan@berkel.fr>
2016-09-13 17:32:33 +0100

    Xcode8 compat changes
    * Add modulemap for MacOSX 10.11
    * Update modulemap
    * Use iPhone SE simulator to fix test failures
      (https://github.com/travis-ci/travis-ci/issues/6422)

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -ev
 if [ -n "$BUILD_SCHEME" ]; then
-    make test BUILD_SCHEME="$BUILD_SCHEME"
+    if [ -n "$IOS_SIMULATOR" ]; then
+        make test BUILD_SCHEME="$BUILD_SCHEME" IOS_SIMULATOR="$IOS_SIMULATOR"
+    else
+        make test BUILD_SCHEME="$BUILD_SCHEME"
+    fi
 elif [ -n "$VALIDATOR_SUBSPEC" ]; then
     cd CocoaPodsTests && make test
 fi
