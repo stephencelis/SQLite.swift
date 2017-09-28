e9d93b5913e3e1326daee7a6bb9acabb874956de
Jan Berkel <jan@berkel.fr>
2017-09-28 14:02:48 +0200

    Missing import

--- Tests/SQLiteTests/QueryTests.swift
+++ Tests/SQLiteTests/QueryTests.swift
@@ -1,4 +1,13 @@
 import XCTest
+#if SQLITE_SWIFT_STANDALONE
+import sqlite3
+#elseif SQLITE_SWIFT_SQLCIPHER
+import SQLCipher
+#elseif os(Linux)
+import CSQLite
+#else
+import SQLite3
+#endif
 @testable import SQLite
 
 class QueryTests : XCTestCase {
