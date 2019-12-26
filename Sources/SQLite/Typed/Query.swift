b6fc14d9174b22c56a05e265f959ddaffae72316
RameshBachiraju <49223707+RameshBachiraju@users.noreply.github.com>
2019-12-26 15:31:30 +0530

    Update Query.swift
    Fixing the prepare method to avoid crash

--- Sources/SQLite/Typed/Query.swift
+++ Sources/SQLite/Typed/Query.swift
@@ -922,9 +922,10 @@ extension Connection {
         let statement = try prepare(expression.template, expression.bindings)
 
         let columnNames = try columnNamesForQuery(query)
-
+        
+        let rows = try statement.failableNext().map { Row(columnNames, $0) }
         return AnySequence {
-            AnyIterator { statement.next().map { Row(columnNames, $0) } }
+            AnyIterator { rows }
         }
     }
     
