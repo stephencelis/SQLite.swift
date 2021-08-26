62fe865ae8b80bab15f373dfeefae04df3b555ea
Nathan Fallet <contact@nathanfallet.me>
2021-08-26 14:33:12 +0200

    Fix linting

--- Sources/SQLite/Core/Statement.swift
+++ Sources/SQLite/Core/Statement.swift
@@ -207,6 +207,7 @@ public protocol FailableIterator: IteratorProtocol {
 
 extension FailableIterator {
     public func next() -> Element? {
+        // swiftlint:disable:next force_cast
         try! failableNext()
     }
 }
