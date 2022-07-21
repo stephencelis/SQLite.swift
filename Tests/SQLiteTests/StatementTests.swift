4442ad0bbd06d2e843a5bb280cc96070aafec2c8
Adam Wulf <adam.wulf@gmail.com>
2022-07-21 15:01:04 -0500

    Adding public method to reset a prepared statement. This is needed so that clients can reset any prepared statements they keep in memory, which allows for transactions to completely finish. without closing transactions (regardless of commit), the wal file cannot be checkpointed and will grow unbounded.

--- Tests/SQLiteTests/StatementTests.swift
+++ Tests/SQLiteTests/StatementTests.swift
@@ -34,4 +34,61 @@ class StatementTests: SQLiteTestCase {
 
         XCTAssertEqual(names.map({ "\($0)@example.com" }), emails.sorted())
     }
+
+    /// Check that a statement reset will close the implicit transaction, allowing wal file to checkpoint
+    func test_reset_statement() throws {
+        // Remove old test db if any
+        let path = "\(NSTemporaryDirectory())/SQLite.swift Tests.sqlite3"
+        try? FileManager.default.removeItem(atPath: path)
+        try? FileManager.default.removeItem(atPath: path + "-shm")
+        try? FileManager.default.removeItem(atPath: path + "-wal")
+
+        // create new db on disk in wal mode
+        let db = try Connection(.uri(path))
+        let url = URL(fileURLWithPath: db.description)
+        XCTAssertEqual(url.lastPathComponent, "SQLite.swift Tests.sqlite3")
+        try db.run("PRAGMA journal_mode=WAL;")
+
+        // create users table
+        try db.execute("""
+            CREATE TABLE users (
+                id INTEGER PRIMARY KEY,
+                email TEXT NOT NULL UNIQUE,
+                age INTEGER,
+                salary REAL,
+                admin BOOLEAN NOT NULL DEFAULT 0 CHECK (admin IN (0, 1)),
+                manager_id INTEGER,
+                created_at DATETIME,
+                FOREIGN KEY(manager_id) REFERENCES users(id)
+            )
+            """
+        )
+
+        // insert single row
+        try db.run("INSERT INTO \"users\" (email, age, admin) values (?, ?, ?)",
+                   "alice@example.com", 1.datatypeValue, false.datatypeValue)
+
+        // prepare a statement and read a single row. This will incremeent the cursor which
+        // prevents the implicit transaction from closing.
+        // https://www.sqlite.org/lang_transaction.html#implicit_versus_explicit_transactions
+        let statement = try db.prepare("SELECT email FROM users")
+        XCTAssert(try statement.step())
+        let blob = statement.row[0] as Blob
+        XCTAssertEqual("alice@example.com", String(bytes: blob.bytes, encoding: .utf8)!)
+
+        // verify that the transaction is not closed, which prevents wal_checkpoints (both explicit and auto)
+        do {
+            try db.run("pragma wal_checkpoint(truncate)")
+            XCTFail("Database should be locked")
+        } catch {
+            // pass
+        }
+
+        // reset the prepared statement, allowing the implicit transaction to close
+        statement.reset()
+
+        // truncate succeeds
+        try db.run("pragma wal_checkpoint(truncate)")
+    }
+
 }
