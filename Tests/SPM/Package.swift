e2472270ab216fe73085b28c64f722a7103004bc
Jan Berkel <jan@berkel.fr>
2026-01-13 20:59:43 +0100

    Use traits for package test

--- Tests/SPM/Package.swift
+++ Tests/SPM/Package.swift
@@ -1,19 +1,26 @@
-// swift-tools-version:5.7
+// swift-tools-version:6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
+let traits: Set<Package.Dependency.Trait>
+#if os(Linux)
+traits = ["SwiftToolchainCSQLite"]
+#else
+traits = [.defaults]
+#endif
+
 let package = Package(
     name: "test",
     platforms: [
-        .iOS(.v11),
+        .iOS(.v12),
         .macOS(.v10_13),
         .watchOS(.v4),
-        .tvOS(.v11)
+        .tvOS(.v12)
     ],
     dependencies: [
         // for testing from same repository
-        .package(path: "../..")
+        .package(path: "../..", traits: traits)
         // normally this would be:
         // .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.15.4")
     ],
